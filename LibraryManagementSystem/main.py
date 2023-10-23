71dd99b708b7e558c15eff00386dcdf050ae46a1
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-10-23 11:25:27 -0300

    up

---
+++ LibraryManagementSystem/main.py
@@ -0,0 +1,29 @@
+# O Senac MS deseja criar um sistema completo de gerenciamento de uma biblioteca virtual usando a linguagem Python. 
+# Você foi contratado para implementar esse sistema utilizando orientação a objetos. Esse sistema deverá fornecer funcionalidades 
+# avançadas para gerenciar livros, usuários e interações entre eles. Cada livro terá informações detalhadas, incluindo título, autor, 
+# ano de publicação, gênero e o número de cópias disponíveis. Cada usuário será identificado por um nome de usuário único e como informação 
+# pessoal, o seu nome completo e CPF.
+# O sistema deverá suportar as seguintes operações:
+
+    # Adicionar um Livro: Os bibliotecários poderão adicionar novos livros à biblioteca, fornecendo todas as informações necessárias, como 
+    # título, autor, ano de publicação, gênero e o número inicial de cópias disponíveis. Os livros recém-adicionados devem estar automaticamente disponíveis.
+    # Atualizar Informações do Livro: Permitir aos bibliotecários atualizar as informações de um livro 
+    # existente, como título, autor, ano de publicação e gênero.
+    # Adicionar um Usuário: Os administradores do sistema poderão criar perfis de usuário, especificando informações pessoais, um nome 
+    # de usuário único, nome completo e CPF para cada usuário.
+    # Pesquisar Livros: Usuários podem pesquisar livros por título, autor ou gênero e receber uma lista de resultados correspondentes.
+    # Empréstimo de Livro: Usuários registrados poderão emprestar livros. O sistema deve reduzir automaticamente o número de cópias 
+    # disponíveis quando um livro for emprestado.
+    # Devolução de Livro: Usuários registrados poderão devolver livros. O sistema deve aumentar automaticamente o número de cópias 
+    # disponíveis quando um livro for devolvido.
+    # Reserva de Livro: Usuários registrados poderão reservar livros que estão atualmente emprestados. Eles receberão uma 
+    # notificação quando o livro estiver disponível para empréstimo.
+    # Listar Livros em Posse de um Usuário: Usuários podem ver a lista de livros emprestados e reservados em seu nome.
+    # Listar Usuários com um Livro em Posse ou Reserva: Os bibliotecários podem verificar quais usuários têm um livro específico em posse ou reservado.
+    # Remover Usuário: Administradores do sistema podem remover usuários e todas as suas informações associadas do sistema.
+    # Listar Livros Disponíveis para Empréstimo: Usuários podem ver a lista de livros atualmente disponíveis para empréstimo, 
+    # juntamente com o número de cópias disponíveis de cada livro.
+    # Histórico de Empréstimos: O sistema deve manter um histórico de todas as transações de empréstimo e devolução, incluindo datas e detalhes do usuário.
+    # Implemente todas essas funcionalidades de forma organizada e eficiente usando classes e métodos. Certifique-se de que o sistema 
+    # seja amigável para o usuário e forneça feedback adequado ao realizar cada operação.
+
---
+++ SumOfCubesOfEvenNumbers/main.py
@@ -0,0 +1,11 @@
+# Em uma tranquila fazenda, um fazendeiro chamado Calebe encontrou um enigma intrigante. O enigma pedia que ele calculasse a soma 
+# dos cubos de todos os números pares em um intervalo específico. A resolução deste enigma era vital para ajudar Calebe a planejar 
+# suas finanças agrícolas. Calebe, conhecido por sua habilidade com números e contabilidade precisa, estava determinado a resolver o 
+# enigma e garantir o sucesso de sua fazenda. No entanto, ele não tinha conhecimento com while, assim ele precisa da sua ajuda para 
+# calcular a soma dos cubos dos números pares no intervalo de A a B.
+# Você deve escrever um algoritmo que, dado o intervalo de números inteiros A e B (onde B > A), calcule e imprima a soma dos cubos de 
+# todos os números pares no intervalo.
+# Exemplo de Entrada:
+# 2 5
+# Exemplo de Saída:
+# 72
